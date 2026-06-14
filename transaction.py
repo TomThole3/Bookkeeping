a21dffe9f25b80e2b8acceb317cc12e54e1d3362
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-14 18:05:07 +0200

    added category parameter

--- transaction.py
+++ transaction.py
@@ -2,7 +2,7 @@
 
 class Transaction:
     
-    def __init__(self, reference, amount, cdt_dbt, date, description, origin_name, origin_iban):
+    def __init__(self, reference, amount, cdt_dbt, date, description, origin_name, origin_iban, category = None):
         self.reference = reference
         self.amount = amount
         self.cdt_dbt = cdt_dbt
@@ -10,4 +10,4 @@ class Transaction:
         self.description = description
         self.origin_name = origin_name
         self.origin_iban = origin_iban
-        
+        self.category = category
