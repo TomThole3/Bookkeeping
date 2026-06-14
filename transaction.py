a635701daee42b0488f78df8bfa566772ac90464
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-14 17:40:43 +0200

    added transaction class

---
+++ transaction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Transaction:
+    
+    def __init__(self, reference, amount, cdt_dbt, date, description, origin_name, origin_iban):
+        self.reference = reference
+        self.amount = amount
+        self.cdt_dbt = cdt_dbt
+        self.date = date
+        self.description = description
+        self.origin_name = origin_name
+        self.origin_iban = origin_iban
+        
