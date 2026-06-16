2f06b7f1c11cd20a767e5317da766e2f1bf7f20f
Tom Thole <tom.thole@ru.nltom.thole@ru.nlhelpgit helpstopgit config user.email tom.thole@student.ru.nl>
2026-06-16 09:18:47 +0200

    added function to change categories

---
+++ categorydialog.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QLabel
+
+class AddCategoryDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Add Category")
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel("New category name:")
+        layout.addWidget(self.label)
+
+        self.input = QLineEdit()
+        layout.addWidget(self.input)
+
+        self.btn = QPushButton("Add")
+        self.btn.clicked.connect(self.accept)
+        layout.addWidget(self.btn)
+
+        self.setLayout(layout)
+
+    def get_value(self):
+        return self.input.text().strip()
+
